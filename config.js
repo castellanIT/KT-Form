8ad8774b65166d2eb5ee25086a72723d4367bc28
Ezhumalai Castellanre <ezhumalai@castellanre.com>
2026-04-17 22:38:25 +0530

    add config

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * S3 / AWS configuration for KT Form.
+ * Replace YOUR_ACCESS_KEY_ID and YOUR_SECRET_ACCESS_KEY with your IAM credentials.
+ * See S3_SETUP.md for bucket and IAM setup.
+ */
+var S3_CONFIG = {
+    region: 'us-east-1',
+    bucketName: 'kt-form-documents',
+    accessKeyId: 'AKIA35BCK6ZHYKA7V2G2',
+    secretAccessKey: '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'
+};
